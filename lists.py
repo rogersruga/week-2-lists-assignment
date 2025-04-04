5a5ddb286520068d1727d3a125763394d206aea9
rogersruga <rogersruga.g@gmail.com>
2025-04-04 10:37:28 +0300

    This is a complete assignment

---
+++ lists.py
@@ -0,0 +1,16 @@
+# numbers = [12, 23, 19, 25]
+
+# data_types = [32, 2.5, "My number is 42"]
+
+
+# print(data_types[-2])
+
+
+#Slicing
+
+# my_list = ["p", "y", "t", "h", "o", "n"]
+# print(my_list[3:])
+
+numbers = [1, 2, 3, 4, 5, 6, 7]
+numbers.append(9)
+print(numbers)
